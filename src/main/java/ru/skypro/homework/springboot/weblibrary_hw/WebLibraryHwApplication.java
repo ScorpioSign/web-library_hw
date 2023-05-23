38a83e1f9beba3282f0e3722fa51a16c4fc44843
ы <nvsemyonova@mail.ru>
2023-05-23 21:41:10 +0300

    Homework

---
+++ src/main/java/ru/skypro/homework/springboot/weblibrary_hw/WebLibraryHwApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.springboot.weblibrary_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/homework/springboot/weblibrary_hw/WebLibraryHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.springboot.weblibrary_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
